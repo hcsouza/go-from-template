4341a67c6deb13880c79533e6f90a9957850e650
Hugo Souza <hugo.souza@ifood.com.br>
2022-12-29 10:24:23 -0300

    feat: improve clone template

--- main.go
+++ main.go
@@ -3,7 +3,9 @@ package main
 import (
 	"fmt"
 	"html/template"
+	"log"
 	"os"
+	"os/exec"
 	"path/filepath"
 	"strings"
 
@@ -15,46 +17,77 @@ type ProjectGenerator struct {
 	PROJECT_NAME        string
 	PROJECT_DESTINATION string
 	TEMPLATE_NAME       string
+	TEMPLATE_GIT_PATH   string
+}
+
+var DEFAULT_TEMPLATES map[string]string
+
+func init() {
+	DEFAULT_TEMPLATES = make(map[string]string)
+	DEFAULT_TEMPLATES["go-rest-api-template"] = "git@github.com:hcsouza/go-rest-api-template.git"
 }
 
 func main() {
 
 	templateName := (&prompter.Prompter{
-		Choices:    []string{"go-rest-api-template"},
+		//Choices:    []string{"go-rest-api-template"},
 		Default:    "go-rest-api-template",
-		Message:    "Choose project template",
+		Message:    "Choose a default project template or an external",
 		IgnoreCase: true,
 	}).Prompt()
 
+	templateGitPath, ok := DEFAULT_TEMPLATES[templateName]
+	if !ok {
+		templateGitPath = (&prompter.Prompter{
+			Message:    "Pass the template git path: git@github.com:user/user.git",
+			IgnoreCase: true,
+		}).Prompt()
+	}
+
 	projectPath := (&prompter.Prompter{
 		Default:    "github.com/user",
-		Message:    "Choose repository path",
+		Message:    "Choose destination repository path",
 		IgnoreCase: true,
 	}).Prompt()
 
 	projectName := (&prompter.Prompter{
-		Default:    "generated",
+		Default:    "new-go-project",
 		Message:    "Choose generated project name",
 		IgnoreCase: true,
 	}).Prompt()
 
+	dirDestination, _ := os.Getwd()
 	projectDestination := (&prompter.Prompter{
-		Default:    "/tmp",
+		Default:    dirDestination,
 		Message:    "Choose destination path",
 		IgnoreCase: true,
 	}).Prompt()
 
-	generated := ProjectGenerator{projectPath, projectName, projectDestination, templateName}
+	generated := ProjectGenerator{projectPath, projectName, projectDestination, templateName, templateGitPath}
+	cloneTemplate(generated)
 	generateProject(generated)
 }
 
 func generateProject(generated ProjectGenerator) {
-	files, err := searchAllFiles(fmt.Sprintf("./%s", generated.TEMPLATE_NAME))
+	files, err := searchAllFiles(fmt.Sprintf("./Templates/%s", generated.TEMPLATE_NAME))
 	if err != nil {
 		fmt.Println("RunSearchError: ", err)
 	}
 	for _, fileName := range files {
 		RunFile(fileName, generated)
+	}
+}
+
+func cloneTemplate(generated ProjectGenerator) {
+	fmt.Println("Cloning the template!!")
+	fmt.Println()
+	dirname, _ := os.Getwd()
+	clonePath := fmt.Sprintf("%s/Templates/%s", dirname, generated.TEMPLATE_NAME)
+	if _, err := os.Stat(clonePath); os.IsNotExist(err) {
+		cmd := exec.Command("git", "clone", generated.TEMPLATE_GIT_PATH, clonePath)
+		if err := cmd.Run(); err != nil {
+			log.Fatal(err)
+		}
 	}
 }
 
@@ -66,6 +99,7 @@ func RunFile(templatePath string, generated ProjectGenerator) {
 	}
 
 	rootPath := strings.Replace(templatePath, generated.TEMPLATE_NAME, generated.PROJECT_NAME, 1)
+	rootPath = strings.Replace(rootPath, "Templates/", "", 1)
 	fileName := strings.Replace(rootPath, ".tmpl", "", 1)
 
 	if len(generated.PROJECT_DESTINATION) > 0 {
@@ -95,7 +129,7 @@ func RunFile(templatePath string, generated ProjectGenerator) {
 func searchAllFiles(searchDir string) ([]string, error) {
 	fileList := make([]string, 0)
 	e := filepath.Walk(searchDir, func(path string, f os.FileInfo, err error) error {
-		if f.IsDir() == false && strings.Contains(path, ".tmpl") == true {
+		if !f.IsDir() && strings.Contains(path, ".tmpl") {
 			fileList = append(fileList, path)
 		}
 		return err
